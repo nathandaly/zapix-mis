9013b41acc3c9d0c7312fccb8e74a17dc8cfc974
Nate <nathandalypretoria@hotmail.co.uk>
2025-07-27 19:09:41 +0100

    :zap: Adds a command to create Filament users
    Introduces a custom Artisan command to simplify the creation of Filament users.
    
    This command provides options for specifying user details such as first name, last name, email, and password, or prompts for them interactively. It also validates the email to avoid duplicates.
    
    Updates user model to use guarded property.

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,11 +19,7 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -46,6 +42,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function getNameAttribute(): string
+    {
+        return trim("{$this->first_name} {$this->last_name}");
     }
 
     /**
